4f1f60cfbec8a1154468ecf02bbb0c027b048e21
Emanuel <emanuel_heitor@hotmail.com>
2022-10-29 16:23:13 -0300

    terceiro commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+import React from "react";
+
+export default function Button ({content}) {
+    return <StyledButton>
+        {content}
+    </StyledButton>
+}
+
+const StyledButton = styled.button`
+
+background: linear-gradient(to right, #142450 0%, #2c4c7c 79%);
+text-transform: uppercase;
+letter-spacing: 0.2rem;
+width: 65%;
+height: 3rem;
+border: none;
+color: white;
+border-radius:2rem;
+cursor: pointer;
+`;
